190ef10c7c78c507d8fde74403a5c6f025fe8c1c
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-06-04 16:10:19 +0100

    fixed

--- src/components/sectionTwo/SectionTwo.jsx
+++ src/components/sectionTwo/SectionTwo.jsx
@@ -3,20 +3,20 @@ import CardOne from "../cardOne/CardOne";
 
 export default function SectionTwo() {
   return (
-    <div className="section pt-[10vh] pb-[5vh] w-[100%] h-[100vh]  bg-[#FBFBFB]">
+    <div className="section pt-[10vh] pb-[5vh] w-full h-[100vh]  bg-[#FBFBFB]">
       <div className="main w-[90%] md:w-[85%] m-auto relative ">
         <div className="title head__two">Veterinarian</div>
         <div className="left absolute top-10 lg:top-[14%] ">
           <CardOne />
         </div>
-        <div className="right absolute bottom-[-40%] md:bottom-[-10%] lg:top-[38%] right-[0%]">
+        <div className="right absolute bottom-[-20%]  lg:bottom-[-10%] lg:top-[38%] right-[0%]">
           <CardOne />
         </div>
         <div className="sectionBg flex items-center justify-center">
           <img
             src={vetImg}
             alt=""
-            className=" pt-[22vh] md:pt-2  md:h-[80vh] object-contain"
+            className=" w-[75vh] pt-[22vh] lg:w-[100vh] md:pt-2  lg:h-[80vh] object-contain"
           />
         </div>
       </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,34 @@
+import CardOne from "../components/cardOne/CardOne";
 import Hero from "../components/hero/Hero";
 import Section from "../components/section/Section";
 import SectionTwo from "../components/sectionTwo/sectionTwo";
-
+import vetClinic from "../assets/bg/vet-clinic.png";
 export default function Home() {
   return (
     <div>
-        <Hero/>
-        <Section/>
-        <SectionTwo/>
+      <Hero />
+      <Section />
+      <SectionTwo />
+      <div className="vetClinic flex pt-[10vh] pb-10 w-[100%] h-[100%] md:h-[100vh] bg-cover bg-[#F4F4FF]">
+        <div className="flexBody hero__body w-[90%] md:w-[85%] h-[100%] m-auto">
+          <div className="title font-black head__two ">Vet Clinic</div>
+          <div className="grouped flex flex-col-reverse md:flex md:flex-row space-x-2  justify-between items-center" >
+            <div className="left">
+              <div className="grouped flex flex-col gap-5">
+                <CardOne />
+                <CardOne />
+              </div>
+            </div>
+            <div className="right">
+              <img
+                src={vetClinic}
+                alt=""
+                className=" h-full w-full object-contain"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
