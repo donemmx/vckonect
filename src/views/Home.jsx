ebd4e17209bfdc3afb363e54f9b286d6eb36b37b
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-06-04 16:25:20 +0100

    fix

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/hero/Hero";
 import Section from "../components/section/Section";
 import SectionTwo from "../components/sectionTwo/sectionTwo";
 import vetClinic from "../assets/bg/vet-clinic.png";
+import vetStore from "../assets/bg/vet-store.png";
 export default function Home() {
   return (
     <div>
@@ -12,7 +13,7 @@ export default function Home() {
       <div className="vetClinic flex pt-[10vh] pb-10 w-[100%] h-[100%] md:h-[100vh] bg-cover bg-[#F4F4FF]">
         <div className="flexBody hero__body w-[90%] md:w-[85%] h-[100%] m-auto">
           <div className="title font-black head__two ">Vet Clinic</div>
-          <div className="grouped flex flex-col-reverse md:flex md:flex-row space-x-2  justify-between" >
+          <div className="grouped flex flex-col-reverse md:flex md:flex-row space-x-2  justify-between">
             <div className="left pt-[4%]">
               <div className="grouped flex flex-col gap-5">
                 <CardOne />
@@ -25,6 +26,24 @@ export default function Home() {
                 alt=""
                 className=" h-full w-full object-contain"
               />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="vetVendor  flex pt-[10vh] pb-10 w-[100%] h-[100%] md:h-[95vh] bg-cover bg-[#FBFBFB]">
+        <div className="body w-[90%] md:w-[85%] h-[100%] m-auto">
+          <div className="title font-black head__two ">Vet Vendor & Stores</div>
+          <div className="grouped flex flex-col md:flex md:flex-row space-x-2  justify-between">
+            <div className="left lg:w-[50%]">
+            <img
+                src={vetStore}
+                alt=""
+                className=" h-full pt-[4%] w-full object-contain"
+              />
+            </div>
+            <div className="right pt-[4%] flex flex-col gap-5" >
+              <CardOne />
+              <CardOne />
             </div>
           </div>
         </div>
