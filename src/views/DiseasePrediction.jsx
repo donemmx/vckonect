789f718de20f67e9ad8cdbfc72c8c8ba67b91e6e
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-07-01 19:25:43 +0100

    fix

--- src/views/DiseasePrediction.jsx
+++ src/views/DiseasePrediction.jsx
@@ -1,20 +1,24 @@
 import { Dropdown } from "primereact/dropdown";
 import { useState } from "react";
-import { InputText } from "primereact/inputtext";
+import { MultiSelect } from "primereact/multiselect";
 
 export default function DiseasePrediction() {
   const [specie, setSpecie] = useState(null);
+  const [symptom, setSymptom] = useState(null);
+
   const animalSpecies = ["Poultry", "Dog", "Swine", "Goat", "Cattle", "Cat"];
+  const symptoms = ["Loss of Appetite", "Weight Loss", "Diarrhea", "Rashes", "Inflammation in the limbs", "Abnormal Posture"];
 
   return (
     <div className="mt-14 lg:mt-4">
       <div className="flex justify-center items-center">
         <div className=" w-[90%] lg:w-[35%] md:w-[60%]">
           <h2 className="title font-black text-center head__two">
-          Disease Predictor
+            Disease Predictor
           </h2>
           <div className="pt-2 subtitle paragraph text-center">
-          Find out what could likely be wrong with your pet or livestock through the noticeable signs and symptoms
+            Find out what could likely be wrong with your pet or livestock
+            through the noticeable signs and symptoms
           </div>
           <div className="form flex flex-col gap-3 pt-6">
             <span className="p-float-label">
@@ -25,8 +29,25 @@ export default function DiseasePrediction() {
                 placeholder=" "
                 className="w-full"
               />
-              <label htmlFor="livestock">Select Animal Specie (Required): </label>
+              <label htmlFor="livestock">
+                Select Animal Specie (Required):{" "}
+              </label>
             </span>
+            { specie !== null ?
+              <span className="p-float-label">
+                <MultiSelect
+                  value={symptom}
+                  onChange={(e) => setSymptom(e.value)}
+                  options={symptoms}
+                  filter
+                  placeholder="Select Cities"
+                  maxSelectedLabels={3}
+                  className="w-full md:w-20rem"
+                />
+                <label htmlFor="livestock">Likely Symptoms (Required): </label>
+              </span>
+              : ''
+            }
             <button className="green__btn" disabled>
               Predict
             </button>
