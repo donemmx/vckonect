1f64721cf426eaa3c2d2233f07b8025cf7bae0a6
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-07-08 17:11:22 +0100

    font

--- src/views/Account.jsx
+++ src/views/Account.jsx
@@ -4,10 +4,11 @@ import callIcon from "../assets/icons/phone-icon.svg";
 import shareIcon from "../assets/icons/share-icon.svg";
 import messageIcon from "../assets/icons/message-icon.svg";
 import markerIcon from "../assets/icons/location-icon.svg";
+import handIcon from "../assets/account/hand-icon.svg";
 
 export default function Account() {
   return (
-    <div className=" bg-white h-[90vh] mb-10  rounded-md border-[1px] border-[#EBEBEB]">
+    <div className=" bg-white h-[110vh] mb-10  rounded-md border-[1px] border-[#EBEBEB]">
       <div className="top bg-account h-[25vh] p-3 lg:p-10 rounded-t-lg">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3 text-[.75rem] lg:text-[.9rem] cursor-pointer">
@@ -35,12 +36,12 @@ export default function Account() {
         Dolapo Adaba
       </div>
 
-      <div className=" text-[.85rem] flex items-center gap-7 justify-center mt-3">
+      <div className=" text-[.82rem] flex items-center gap-7 justify-center mt-3">
         <div className="flex flex-col items-center justify-center ">
           <img
             src={callIcon}
             alt=""
-            className=" p-2 mb-2 h-[30px] w-[30px] bg-white rounded-full shadow"
+            className=" p-2 mb-2 h-[35px] w-[35px] bg-white rounded-full border-[1px] border-[#EBEBEB] shadow"
           />
           Call
         </div>
@@ -48,7 +49,7 @@ export default function Account() {
           <img
             src={messageIcon}
             alt=""
-            className=" p-2 mb-2 h-[30px] w-[30px] bg-white rounded-full shadow"
+            className=" p-2 mb-2 h-[35px] w-[35px] bg-white rounded-full border-[1px] border-[#EBEBEB] shadow"
           />
           Email
         </div>
@@ -56,7 +57,7 @@ export default function Account() {
           <img
             src={markerIcon}
             alt=""
-            className=" p-2 mb-2 h-[30px] w-[30px] bg-white rounded-full shadow"
+            className=" p-2 mb-2 h-[35px] w-[35px] bg-white rounded-full border-[1px] border-[#EBEBEB] shadow"
           />
           Location
         </div>
@@ -64,10 +65,16 @@ export default function Account() {
           <img
             src={shareIcon}
             alt=""
-            className=" p-2  mb-2 h-[30px] w-[30px] bg-white rounded-full shadow"
+            className=" p-2  mb-2 h-[35px] w-[35px] bg-white rounded-full border-[1px] border-[#EBEBEB] shadow"
           />
           Share
         </div>
+      </div>
+
+      <div className="user flex flex-col justify-center items-center mt-[15vh]">
+        <img src={handIcon} alt="" className=" w-[30px]"/>
+        <h4 className=" font-bold pt-3">Hey! Users</h4>
+        <p className="text-sm w-[20%] text-center text-[#666666]">Kindly click any of your choice from the icon button above to connect with this user</p>
       </div>
     </div>
   );
