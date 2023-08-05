0d465ce6bd567c20e718cf81d0355678bfc2fe53
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-05 15:22:06 +0100

    fix

--- src/validations/UserValidation.js
+++ src/validations/UserValidation.js
@@ -30,12 +30,89 @@ const loginUser =  yup.object().shape({
     password: yup.string().required("Required"),
 })
 
+const forumChat = yup.object().shape({
+    title: yup.string().required("Required"),
+    content: yup.string().required("Required"),
+})
+const product = yup.object().shape({
+    category: yup.string().required("Required"),
+    description: yup.string().required("Required"),
+    title: yup.string().required("Required"),
+    tags: yup.string().required("Required"),
+    location: yup.string().required("Required"),
+    price: yup.string().required("Required"),
+    availability: yup.string().required("Required"),
+    unit: yup.string().required("Required"),
+})
 
+const store = yup.object().shape({
+    phone: yup.string().required("Required"),
+    email: yup.string().required("Required"),
+    location: yup.string().required("Required"),
+    availability: yup.string().required("Required"),
+})
 
+const farm = yup.object().shape({
+    farmName: yup.string().required("Required"),
+    workers: yup.string().required("Required"),
+    location: yup.string().required("Required"),
+    livestockType: yup.string().required("Required"),
+    livestock: yup.string().required("Required"),
+    age: yup.string().required("Required"),
+    sex: yup.string().required("Required"),
+})
+
+const clinic = yup.object().shape({
+    licenseNumber: yup.string().required("Required"),
+    clinicName: yup.string().required("Required"),
+    speciality: yup.string().required("Required"),
+    email: yup.string().required("Required"),
+    phone: yup.string().required("Required"),
+    location: yup.string().required("Required"),
+    availability: yup.string().required("Required"),
+})
+
+const addCase = yup.object().shape({
+    clientName: yup.string().required("Required"),
+    phone: yup.string().required("Required"),
+    caseType: yup.string().required("Required"),
+})
+
+const addPetCase  =  yup.object().shape({
+    clientName: yup.string().required("Required"),
+    phone: yup.string().required("Required"),
+    caseType: yup.string().required("Required"),
+    petName: yup.string().required("Required"),
+    specie: yup.string().required("Required"),
+    breed: yup.string().required("Required"),
+    age: yup.string().required("Required"),
+    sex: yup.string().required("Required"),
+    caseTitle: yup.string().required("Required"),
+})
+
+const addFarmCase  =  yup.object().shape({
+    clientName: yup.string().required("Required"),
+    phone: yup.string().required("Required"),
+    caseType: yup.string().required("Required"),
+    petName: yup.string().required("Required"),
+    specie: yup.string().required("Required"),
+    breed: yup.string().required("Required"),
+    age: yup.string().required("Required"),
+    sex: yup.string().required("Required"),
+    caseTitle: yup.string().required("Required"),
+})
 
 export {
     userOne,
     userTwo,
     vetUser,
-    loginUser
+    loginUser,
+    forumChat,
+    product,
+    store,
+    farm,
+    clinic,
+    addCase,
+    addPetCase,
+    addFarmCase
 }
