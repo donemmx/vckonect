8200ee6e8a286541671e7d1fe1b644bc22c2935f
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-06 15:19:33 +0100

    fix

--- src/views/user/AddForumPost.jsx
+++ src/views/user/AddForumPost.jsx
@@ -1,6 +1,6 @@
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createForumChat } from "../../utils/userApiService";
 import { useRecoilValue } from "recoil";
 import { user } from "../../atom/userAtom";
@@ -10,12 +10,40 @@ import { v4 } from "uuid";
 import { toast } from "react-toastify";
 
 export default function AddForumPost() {
+  const imageMimeType = /image\/(png|jpg|jpeg)/i;
+  const [fileDataURL, setFileDataURL] = useState(null);
+
   const userData = useRecoilValue(user);
   const [file, setFile] = useState(null);
   function getImage(e) {
-    console.log(e.target.files);
-    setFile(URL.createObjectURL(e.target.files[0]));
+    const file = e.target.files[0];
+    if (!file.type.match(imageMimeType)) {
+      alert("Image mime type is not valid");
+      return;
+    }
+    setFile(file);
   }
+
+  useEffect(() => {
+    let fileReader,
+      isCancel = false;
+    if (file) {
+      fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const { result } = e.target;
+        if (result && !isCancel) {
+          setFileDataURL(result);
+        }
+      };
+      fileReader.readAsDataURL(file);
+    }
+    return () => {
+      isCancel = true;
+      if (fileReader && fileReader.readyState === 1) {
+        fileReader.abort();
+      }
+    };
+  }, [file]);
 
   const onSubmit = async (values) => {
     const payload = {
@@ -26,8 +54,8 @@ export default function AddForumPost() {
     };
     await createForumChat(payload)
       .then(() => {
-       toast.success('Post added successfully')
-       window.history.back()
+        toast.success("Post added successfully");
+        window.history.back();
       })
       .catch((err) => console.log(err));
   };
@@ -48,7 +76,10 @@ export default function AddForumPost() {
         {" "}
         <span className="underline">Forum Chat</span> / Add to Forum
       </div>
-      <form onSubmit={handleSubmit} className=" bg-white h-full p-5 flex flex-col gap-8 rounded-md border-[1px] border-[#EBEBEB]">
+      <form
+        onSubmit={handleSubmit}
+        className=" bg-white h-full p-5 flex flex-col gap-8 rounded-md border-[1px] border-[#EBEBEB]"
+      >
         <div className=" font-black text-2xl">Add to Forum Chat</div>
         <span className="p-float-label">
           <InputText
@@ -61,8 +92,8 @@ export default function AddForumPost() {
           <label htmlFor="username">Title</label>
         </span>
         {errors.title && touched.title && (
-            <p className="error">{errors.title}</p>
-          )}
+          <p className="error">{errors.title}</p>
+        )}
         <span className="p-float-label">
           <InputTextarea
             name="content"
@@ -76,23 +107,28 @@ export default function AddForumPost() {
           <label htmlFor="username">Contents</label>
         </span>
         {errors.content && touched.content && (
-            <p className="error">{errors.content}</p>
-          )}
-        {file !== null ? (
+          <p className="error">{errors.content}</p>
+        )}
+        {fileDataURL !== null ? (
           <>
             <img
-              src={file}
+              src={fileDataURL}
               className="h-[200px] w-full object-cover border-[1px] rounded-md"
             />
             <div
               className="underline cursor-pointer"
-              onClick={() => setFile(null)}
+              onClick={() => setFileDataURL(null)}
             >
               Remove Image
             </div>
           </>
         ) : (
-          <input type="file" onChange={getImage} />
+          <input
+            type="file"
+            id="image"
+            accept=".png, .jpg, .jpeg"
+            onChange={getImage}
+          />
         )}
         <button className="green__btn">Submit</button>
       </form>
