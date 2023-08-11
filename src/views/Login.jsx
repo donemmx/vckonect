c615dd4ff0f8c4c1d58235f23ac1964d38904189
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-11 11:45:25 +0100

    fix

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -127,7 +127,7 @@ export default function Login() {
             client_secret={linkedinSecretId}
             redirect_uri={'https://vetkonect.com/backend/public/api/linkedin/callback?token=AQVZmfOTTXG0UL1fh1qtEah0TrGI7gvGyl7tTWI9iekJwP59CEMwYbT4SS6UzqhgjWesZ3AOYoYkIRO90KCMDn3YRLq_CnQi9kXl7MiUDcxVslzvE9jP0ERfxr-KULHiRIRwwsoBdUxLq36UIBAyQdRSF3iu0bxzKnBHOdYsqr_ZfLM6JU5aALJa4bAJNium-hWCMBaZsbuVnGXnuum-7b9Y_p8y8qZ58b7owtPveNzjzPHA-OKLW-ub0prfkqVSqkc5G1RMxfGLzXDZmIZ8FKWOFdlDc2WQj5EKdT_Ah0YYTDP4bslluOfAif-9zzOP96nb33EMeN_AckBWY53qhvMHO_KBSg'}
             onResolve={({ data }) => {
-              linkedInCallback(data.access_token).then((res)=> {
+              linkedInCallback('AQVZmfOTTXG0UL1fh1qtEah0TrGI7gvGyl7tTWI9iekJwP59CEMwYbT4SS6UzqhgjWesZ3AOYoYkIRO90KCMDn3YRLq_CnQi9kXl7MiUDcxVslzvE9jP0ERfxr-KULHiRIRwwsoBdUxLq36UIBAyQdRSF3iu0bxzKnBHOdYsqr_ZfLM6JU5aALJa4bAJNium-hWCMBaZsbuVnGXnuum-7b9Y_p8y8qZ58b7owtPveNzjzPHA-OKLW-ub0prfkqVSqkc5G1RMxfGLzXDZmIZ8FKWOFdlDc2WQj5EKdT_Ah0YYTDP4bslluOfAif-9zzOP96nb33EMeN_AckBWY53qhvMHO_KBSg').then((res)=> {
                 console.log(res);
               })
             }}
