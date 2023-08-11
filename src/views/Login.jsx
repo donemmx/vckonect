488f3ad65eaee7eb87ba7f540aa36be9d42a52f3
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-11 16:38:15 +0100

    fix

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -125,6 +125,7 @@ export default function Login() {
             <LoginSocialLinkedin
             client_id={linkedinClientId}
             client_secret={linkedinSecretId}
+            state="_linkedin"
             redirect_uri='https://vetkonect.com/backend/public/api/linkedin/callback'
             onResolve={({ data }) => {
               linkedInCallback().then((res)=> {
