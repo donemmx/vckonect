aaf226c2469663a19d47f2c0ca86966715212ea6
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-12 16:24:11 +0100

    fix

--- src/views/user/PetandLiveStock.jsx
+++ src/views/user/PetandLiveStock.jsx
@@ -64,7 +64,7 @@ export default function PetandLiveStock() {
 
             {allPets.map((res) => (
               <PeLivestocktCard
-                petImg={dogImg}
+                petImg={res.picture}
                 specie={res.specie}
                 breed={res.breed}
                 sex={res.sex}
