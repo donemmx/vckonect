4f0b7389201056b2c3bf8f138dc08368854c59c4
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-12 12:22:50 +0100

    fix

--- src/views/user/FeedCalculator.jsx
+++ src/views/user/FeedCalculator.jsx
@@ -10,6 +10,7 @@ export default function FeedCalculator() {
   const [noOfPigs, setNoOfPigs] = useState(null);
   const [bridType, setBridType] = useState(null);
   const [weeks, setWeeks] = useState(null);
+  const [result, setResult] = useState(null);
   const [noOfBirds, setNoOfBirds] = useState(null);
   const [feedType, setFeedType] = useState(null);
   const livestocks = [
@@ -22,44 +23,46 @@ export default function FeedCalculator() {
   const broilersFeedTypes = ["Starter", "Finisher"];
   const layersFeedTypes = ["Chick Mash", "Growers Mash", "Layers Mash/Pellet"];
 
+  const restart = () => {
+    setResult(null);
+  };
+
   let handleSubmit = (e) => {
-    e.preventDefault()
-    let payload
-    if(livestock == 'Poultry'){
-      if(bridType == 'Layers'){
+    e.preventDefault();
+    let payload;
+    if (livestock == "Poultry") {
+      if (bridType == "Layers") {
         payload = {
-         livestock_category: livestock,
-         bird_type: bridType,
-         feed_type: feedType,
-         no_of_bird: noOfBirds
-       }
-      }
-      else{
+          livestock_category: livestock,
+          bird_type: bridType,
+          feed_type: feedType,
+          no_of_bird: noOfBirds,
+        };
+      } else {
         payload = {
           livestock_category: livestock,
           bird_type: bridType,
           feed_type: feedType,
           no_of_week: weeks,
-          no_of_bird: noOfBirds
-        }
+          no_of_bird: noOfBirds,
+        };
       }
-    }
-    else if(livestock == 'Fish'){
+    } else if (livestock == "Fish") {
       payload = {
         livestock_category: livestock,
-        no_of_fish: noOfFish
-      }
-    }
-    else {
+        no_of_fish: noOfFish,
+      };
+    } else {
       payload = {
         livestock_category: livestock,
-        no_of_pig: noOfPigs
-      }
+        no_of_pig: noOfPigs,
+      };
     }
 
-    feedCalculator(payload).then((res)=> console.log(res))
-  }
-
+    feedCalculator(payload).then((res) => {
+      setResult(res);
+    });
+  };
 
   return (
     <div className="mt-14 lg:mt-4">
@@ -71,106 +74,183 @@ export default function FeedCalculator() {
           <div className="pt-2 subtitle paragraph text-center">
             Calculate Feed for your pets and livestocks
           </div>
-          <form onSubmit={handleSubmit} className="form flex flex-col gap-3 pt-6">
-            <span className="p-float-label">
-              <Dropdown
-                value={livestock}
-                onChange={(e) => setlivestock(e.value)}
-                options={livestocks}
-                optionLabel="name"
-                placeholder=" "
-                className="w-full"
-              />
-              <label htmlFor="livestock">Livestock Category (Required): </label>
-            </span>
-            {livestock === "Poultry" ? (
+          {!result ? (
+            <form
+              onSubmit={handleSubmit}
+              className="form flex flex-col gap-3 pt-6"
+            >
               <span className="p-float-label">
                 <Dropdown
-                  value={bridType}
-                  onChange={(e) => setBridType(e.value)}
-                  options={bridTypes}
+                  value={livestock}
+                  onChange={(e) => setlivestock(e.value)}
+                  options={livestocks}
+                  optionLabel="name"
                   placeholder=" "
                   className="w-full"
                 />
-                <label htmlFor="livestock">Bird Type (Required): </label>
+                <label htmlFor="livestock">
+                  Livestock Category (Required):{" "}
+                </label>
               </span>
-            ) : (
-              ""
-            )}
-            {livestock === "Poultry" && bridType === "Broilers" ? (
-              <span className="p-float-label">
-                <Dropdown
-                  value={feedType}
-                  onChange={(e) => setFeedType(e.value)}
-                  options={broilersFeedTypes}
-                  placeholder=" "
-                  className="w-full"
-                />
-                <label htmlFor="livestock">Feed Type (Required): </label>
-              </span>
-            ) : (
-              ""
-            )}
-            {livestock === "Poultry" && bridType === "Layers" ? (
-              <span className="p-float-label">
-                <Dropdown
-                  value={feedType}
-                  onChange={(e) => setFeedType(e.value)}
-                  options={layersFeedTypes}
-                  placeholder=" "
-                  className="w-full"
-                />
-                <label htmlFor="livestock">Feed Type (Required): </label>
-              </span>
-            ) : (
-              ""
-            )}
-            {livestock === "Poultry" && bridType !== null ? (
-              <>
-                {bridType === "Broilers" ? (
+              {livestock === "Poultry" ? (
+                <span className="p-float-label">
+                  <Dropdown
+                    value={bridType}
+                    onChange={(e) => setBridType(e.value)}
+                    options={bridTypes}
+                    placeholder=" "
+                    className="w-full"
+                  />
+                  <label htmlFor="livestock">Bird Type (Required): </label>
+                </span>
+              ) : (
+                ""
+              )}
+              {livestock === "Poultry" && bridType === "Broilers" ? (
+                <span className="p-float-label">
+                  <Dropdown
+                    value={feedType}
+                    onChange={(e) => setFeedType(e.value)}
+                    options={broilersFeedTypes}
+                    placeholder=" "
+                    className="w-full"
+                  />
+                  <label htmlFor="livestock">Feed Type (Required): </label>
+                </span>
+              ) : (
+                ""
+              )}
+              {livestock === "Poultry" && bridType === "Layers" ? (
+                <span className="p-float-label">
+                  <Dropdown
+                    value={feedType}
+                    onChange={(e) => setFeedType(e.value)}
+                    options={layersFeedTypes}
+                    placeholder=" "
+                    className="w-full"
+                  />
+                  <label htmlFor="livestock">Feed Type (Required): </label>
+                </span>
+              ) : (
+                ""
+              )}
+              {livestock === "Poultry" && bridType !== null ? (
+                <>
+                  {bridType === "Broilers" ? (
+                    <span className="p-float-label">
+                      <InputText
+                        id="username"
+                        value={weeks}
+                        onChange={(e) => setWeeks(e.target.value)}
+                      />
+                      <label htmlFor="usernam">
+                        Number of Weeks (Required)
+                      </label>
+                    </span>
+                  ) : (
+                    ""
+                  )}
                   <span className="p-float-label">
-                    <InputText id="username" value={weeks} onChange={(e)=> setWeeks(e.target.value)} />
-                    <label htmlFor="usernam" >Number of Weeks (Required)</label>
+                    <InputText
+                      id="username"
+                      value={noOfBirds}
+                      onChange={(e) => setNoOfBirds(e.target.value)}
+                    />
+                    <label htmlFor="username">Number of Birds (Required)</label>
                   </span>
-                ) : (
-                  ""
-                )}
-                <span className="p-float-label">
-                  <InputText id="username" value={noOfBirds} onChange={(e) => setNoOfBirds(e.target.value)} />
-                  <label htmlFor="username" >Number of Birds (Required)</label>
-                </span>
-              </>
-            ) : (
-              ""
-            )}
-            {livestock === "Fish" ? (
-              <>
-                <span className="p-float-label">
-                  <InputText id="username"   value={fishSize} onChange={(e) => setFishSize(e.target.value)}/>
-                  <label htmlFor="username" >Size of Fish (Required): </label>
-                </span>
-                <span className="p-float-label">
-                  <InputText id="username"  value={noOfFish} onChange={(e) => setNoOfFish(e.target.value)}/>
-                  <label htmlFor="username" >Number of Fish (Required): </label>
-                </span>
-              </>
-            ) : (
-              ""
-            )}
-            {livestock === "Pig" ? (
-              <>
-                <span className="p-float-label">
-                  <InputText id="username"  value={noOfPigs} onChange={(e) => setNoOfPigs(e.target.value)} />
-                  <label htmlFor="username" >Number of Pigs (Required): </label>
-                </span>
-              </>
-            ) : (
-              ""
-            )}
-            <button type="submit" className="green__btn">
-              Submit
-            </button>
-          </form>
+                </>
+              ) : (
+                ""
+              )}
+              {livestock === "Fish" ? (
+                <>
+                  <span className="p-float-label">
+                    <InputText
+                      id="username"
+                      value={fishSize}
+                      onChange={(e) => setFishSize(e.target.value)}
+                    />
+                    <label htmlFor="username">Size of Fish (Required): </label>
+                  </span>
+                  <span className="p-float-label">
+                    <InputText
+                      id="username"
+                      value={noOfFish}
+                      onChange={(e) => setNoOfFish(e.target.value)}
+                    />
+                    <label htmlFor="username">
+                      Number of Fish (Required):{" "}
+                    </label>
+                  </span>
+                </>
+              ) : (
+                ""
+              )}
+              {livestock === "Pig" ? (
+                <>
+                  <span className="p-float-label">
+                    <InputText
+                      id="username"
+                      value={noOfPigs}
+                      onChange={(e) => setNoOfPigs(e.target.value)}
+                    />
+                    <label htmlFor="username">
+                      Number of Pigs (Required):{" "}
+                    </label>
+                  </span>
+                </>
+              ) : (
+                ""
+              )}
+              <button type="submit" className="green__btn">
+                Submit
+              </button>
+            </form>
+          ) : (
+            <div className="mt-5 shadow-xl rounded-lg p-5" >
+              <h3 className="text-[60px] font-black text-center mt-4 text-[#1D2432]">
+                {result.weight}
+              </h3>
+              <p className="p-2 text-center text-[#555555] text-md mt-2">
+                Results
+              </p>
+              <p className="text-center text-[#555555] mt-5 text-md">
+                Livestock Category
+              </p>
+              <p className="text-center text-md font-bold text-[#1D2432]">
+                {result.livestock_category}
+              </p>
+              <p className="text-center text-[#555555] mt-5 mb-3 text-md">
+                Birds Type
+              </p>
+              <p className="text-center text-md font-bold text-[#1D2432]">
+                {result.bird_type}
+              </p>
+              <p className="text-center text-[#555555] mt-5 text-md">
+                Feed Type
+              </p>
+              <p className="text-center text-md font-bold text-[#1D2432]">
+                {result.feed_type}
+              </p>
+              <p className="text-center text-[#555555] mt-5 text-md">
+                Number of Birds{" "}
+              </p>
+              <p className="text-center text-md font-bold text-[#1D2432]">
+                {result.no_of_bird}
+              </p>
+              <p className="text-center text-[#555555] mt-5 text-md">Space </p>
+              <p className="text-center text-md font-bold text-[#1D2432]">
+                {result.space}
+              </p>
+
+              <div className=" grid gap-2 mt-10">
+                <button className="tertiary__btn">Consult a Vet</button>
+                <button className="green__btn" onClick={restart}>
+                  Restart
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
