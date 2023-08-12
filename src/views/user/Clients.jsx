09e7edfbd881b719e1e5449b6b4bb95c97210844
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-12 10:41:45 +0100

    fix

--- src/views/user/Clients.jsx
+++ src/views/user/Clients.jsx
@@ -1,15 +1,28 @@
 import ClientCard from "../../components/clientCard/ClientCard";
 import user2 from "../../assets/icons/user-2.png";
 import user3 from "../../assets/icons/user-3.png";
+import { useEffect, useState } from "react";
+import { getClient } from "../../utils/userApiService";
+import { useRecoilValue } from "recoil";
+import { user } from "../../atom/userAtom";
 export default function Clients() {
+  const userData = useRecoilValue(user);
+  const [loading, setLoading] = useState(true);
+  const [allClients, SetAllClients] = useState([]);
+
+  useEffect(() => {
+    getClient({ id: userData.id }).then((res) => {
+      SetAllClients(res);
+      setLoading(false);
+    });
+  }, []);
 
   return (
-  <div className="">
-        <div className=" font-black text-2xl">My Clients</div>
-
-      <ClientCard user={user2}  name='Dr. Amechi'  />
-      <ClientCard user={user3} name='Treequote Store'  />
-  </div>
-  )
-  
+    <div className="">
+      <div className=" font-black text-2xl">My Clients</div>
+      {allClients.map((res) => (
+        <ClientCard key={res.id} user={user2} name="Dr. Amechi" />
+      ))}
+    </div>
+  );
 }
