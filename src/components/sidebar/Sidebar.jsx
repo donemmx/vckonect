4fdb60b1ad33d79794390b451d2a444729e8d300
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-19 16:24:43 +0100

    fix

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,10 @@
-import homeicon from "../../assets/sidebar/home.svg";
 import dashicon from "../../assets/sidebar/dashboard.svg";
 import livestockicon from "../../assets/sidebar/livestock.svg";
-import clienticon from "../../assets/sidebar/clients.svg";
-import storeicon from "../../assets/sidebar/stores.svg";
 import chaticon from "../../assets/sidebar/chat.svg";
-import feedicon from "../../assets/sidebar/feed-calc.svg";
-import diseaseicon from "../../assets/sidebar/disease-prediction.svg";
 import accounticon from "../../assets/sidebar/account.svg";
-import clinicIcon from "../../assets/sidebar/clinic-dash.svg";
-import casesIcon from "../../assets/sidebar/cases.svg";
+import logout from "../../assets/sidebar/logout.svg";
+import users from "../../assets/sidebar/users.svg";
+import subscription from "../../assets/sidebar/subscription.svg";
 import adsicon from "../../assets/sidebar/ads.svg";
 import activitiesicon from "../../assets/sidebar/activities.svg";
 import { Link } from "react-router-dom";
@@ -25,7 +21,7 @@ export default function Sidebar() {
   }, []);
   return (
     <div className="fixed flex items-center justify-center z-50 bg-white w-[17vw] l-0 top-0 h-[100vh]">
-      <div className="flex flex-col items-center mt-[12vh] h-[80%] gap-1">
+      <div className="flex flex-col items-center mt-[12vh] h-[80%] gap-2">
         <Link to="/" className="logo h-[28px] absolute top-[5%] ">
           <img
             src={logo}
@@ -39,23 +35,23 @@ export default function Sidebar() {
           className="flex items-center w-[100%]  gap-4 cursor-pointer   p-2   "
         >
           <img className="h-[30px]" src={dashicon} alt="" />
-          <small className="text-[12px] leading-3 font-bold">Dashboard</small>
+          <small className="text-[12px] leading-3 ">Dashboard</small>
         </Link>
         <Link
           to="/user-features"
           className="flex items-center w-[100%]  gap-4 cursor-pointer  p-2   "
         >
-          <img className="h-[30px]" src={casesIcon} alt="" />
-          <small className="text-[12px] leading-3 text-center font-bold">
+          <img className="h-[30px]" src={users} alt="" />
+          <small className="text-[12px] leading-3 text-center ">
             Users & Features
           </small>
         </Link>
         <Link
-          to="/clinic"
+          to="/forum-content"
           className=" flex items-center w-[100%]  gap-4  cursor-pointer p-2   "
         >
-          <img className="h-[30px]" src={clinicIcon} alt="" />
-          <small className="text-[12px] leading-3 text-center font-bold">
+          <img className="h-[30px]" src={chaticon} alt="" />
+          <small className="text-[12px] leading-3 text-center ">
             Forum Contents
           </small>
         </Link>
@@ -63,8 +59,8 @@ export default function Sidebar() {
           to="/subscriptions"
           className=" flex items-center w-[100%]  gap-4  cursor-pointer   p-2   "
         >
-          <img className="h-[30px]" src={livestockicon} alt="" />
-          <small className="text-[12px] leading-3 text-center font-bold">
+          <img className="h-[30px]" src={subscription} alt="" />
+          <small className="text-[12px] leading-3 text-center ">
             Subscriptions
           </small>
         </Link>
@@ -72,8 +68,8 @@ export default function Sidebar() {
           to="/admin-promotion"
           className=" flex items-center w-[100%]  gap-4  cursor-pointer p-2   "
         >
-          <img className="h-[30px]" src={clienticon} alt="" />
-          <small className="text-[12px] leading-3 font-bold text-center">
+          <img className="h-[30px]" src={adsicon} alt="" />
+          <small className="text-[12px] leading-3  text-center">
             Promotion
           </small>
         </Link>
@@ -81,18 +77,9 @@ export default function Sidebar() {
           to="/admin-activity"
           className="flex items-center w-[100%]  gap-4 cursor-pointer  p-2   "
         >
-          <img className="h-[30px]" src={storeicon} alt="" />
-          <small className="text-[12px] leading-3 font-bold text-center">
+          <img className="h-[30px]" src={activitiesicon} alt="" />
+          <small className="text-[12px] leading-3  text-center">
             Activity History
-          </small>
-        </Link>
-        <Link
-          to="/forum"
-          className="flex items-center w-[100%]  gap-4 cursor-pointer   p-2   "
-        >
-          <img className="h-[30px]" src={chaticon} alt="" />
-          <small className="text-[12px] leading-3 font-bold text-center">
-            Chat Forum
           </small>
         </Link>
         <Link
@@ -100,13 +87,13 @@ export default function Sidebar() {
           className=" flex items-center w-[100%]  gap-4 cursor-pointer   p-2   "
         >
           <img className="h-[30px]" src={accounticon} alt="" />
-          <small className="text-[12px] leading-3 font-bold text-center">
+          <small className="text-[12px] leading-3  text-center">
             My Account
           </small>
         </Link>
         <Link className=" flex items-center w-[100%]  gap-4 cursor-pointer  p-2   ">
-          <img className="h-[30px]" src={adsicon} alt="" />
-          <small className="text-[12px] leading-3 font-bold text-center">
+          <img className="h-[30px]" src={logout} alt="" />
+          <small className="text-[12px] leading-3  text-center">
             Logout
           </small>
         </Link>
