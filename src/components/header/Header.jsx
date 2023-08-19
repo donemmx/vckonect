44d60e86bd413c236729200aa8800ff8a0e7c0c7
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-19 19:13:21 +0100

    fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -46,7 +46,7 @@ export default function Header({ bg }) {
           bg ? "bg-hero" : " bg-white"
         }  w-[100vw] h-[80px] flex justify-center items-center fixed z-[100] `}
       >
-        <div className="header flex items-center justify-between p-2 w-[90%] md:w-[85%] m-auto">
+        <div className="header flex items-center justify-between p-1 w-[90%] md:w-[85%] m-auto">
           <Link to="/" className="logo h-[25px] md:h-[30px] lg:h-[35px]">
             <img
               src={logo}
