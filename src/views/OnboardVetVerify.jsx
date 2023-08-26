1d54f9de1ac87a4fc563573cc1610da9c5c1a646
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-08-26 14:58:44 +0100

    fix

--- src/views/OnboardVetVerify.jsx
+++ src/views/OnboardVetVerify.jsx
@@ -116,7 +116,7 @@ export default function OnboardVetVerify() {
           </Link>
         </form>
 
-        <SubscriptionCard/>
+        {/* <SubscriptionCard/> */}
       </div>
     </div>
   );
