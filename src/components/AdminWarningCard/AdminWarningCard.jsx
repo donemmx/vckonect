ca7222f1262256584f4d732ee73ba9c0bc2db2b6
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-09-15 16:17:38 +0100

    fix

--- src/components/AdminWarningCard/AdminWarningCard.jsx
+++ src/components/AdminWarningCard/AdminWarningCard.jsx
@@ -7,20 +7,29 @@ export default function AdminWarningCard({
   acceptFunction,
   rejcetButtonText,
   approveButtonText,
+  verifyVetFunction,
   loading,
   id,
   vet,
   selectedId,
 }) {
   const [visible, setVisible] = useState(false);
+  const [verifyVisible, setVerifyVisible] = useState(false);
 
   const openModal = () => {
     setVisible(!visible);
+  };
+  const openVerifyModal = () => {
+    setVerifyVisible(!verifyVisible);
   };
 
   const accept = async () => {
     openModal();
     acceptFunction();
+  };
+  const verifyVet = async () => {
+    openVerifyModal();
+    verifyVetFunction();
   };
 
   return (
@@ -53,10 +62,38 @@ export default function AdminWarningCard({
           </div>
         </div>
       </Dialog>
+      <Dialog
+        visible={verifyVisible}
+        draggable={false}
+        header={header}
+        className=" w-[95%] md:w-[70%] lg:w-[30%]"
+        onHide={() => setVerifyVisible(false)}
+      >
+        <div className="">
+          <div className="warning mb-5">
+            <i className="pi pi-exclamation-triangle !text-[30px] !flex !items-center !justify-center !mb-5 text-red-600 "></i>
+            <p className="mt-5 text-center py-3">Are you sure you want to verify this vet?</p>
+          </div>
+          <div className="flex items-center justify-center gap-4">
+            <button
+              className="border border-green-600 p-3 rounded px-4 text-sm flex items-center gap-2 text-green-700 hover:bg-green-700 hover:text-white "
+              onClick={verifyVet}
+            >
+              <i className="pi pi-check"></i>Yes, Proceed
+            </button>
+            <button
+              className="border border-green-600 p-3 rounded px-4 text-sm flex items-center gap-2 text-green-700 hover:bg-green-700 hover:text-white"
+              onClick={openVerifyModal}
+            >
+              <i className="pi pi-times"></i>No, Cancel
+            </button>
+          </div>
+        </div>
+      </Dialog>
       <div className="flex items-center gap-2">
         {vet === "Not Verified" ? (
           <>
-            <button className=" border flex items-center gap-2 justify-center rounded-full w-[110px] h-[30px] text-green-800 bg-green-50 border-green-400 text-xs">
+            <button className=" border flex items-center gap-2 justify-center rounded-full w-[110px] h-[30px] text-green-800 bg-green-50 border-green-400 text-xs" onClick={openVerifyModal}>
               Verify Vet No.
             </button>
           </>
