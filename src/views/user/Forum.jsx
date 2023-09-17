30994973cd4912188be322156b244786d1cce467
Ebencharger <ajayioluwaseunebenezer@gmail.com>
2023-09-17 04:29:09 +0100

    fix

--- src/views/user/Forum.jsx
+++ src/views/user/Forum.jsx
@@ -48,20 +48,21 @@ export default function Forum() {
     });
     const channel = pusher.subscribe("chatbox");
     channel.bind("App\\Events\\DirectMessage", (data) => {
-      console.log(data);
+      console.log(messages);
       if (
         (data.sender_id == userData.id && data.sender_role == userData.role) ||
         (data.receiver_id == userData.id &&
           data.receiver_role == userData.receiver_role)
       ) {
-        setMessages([...messages, data]);
-        console.log(messages);
+        let message=[...messages, data]
+        setMessages(message);
+        console.log(message);
       }
     });
     return () => {
       pusher.unsubscribe("chatbox");
     };
-  }, []);
+  }, [messages]);
 
   useEffect(() => {
     const all = allmessages;
