0b2d91f19306a3aa4dd7466d93fac0d4487580e8
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-09-20 09:51:02 +0100

    fix

--- src/components/adminDashboardCard/AdminDashboardCard.jsx
+++ src/components/adminDashboardCard/AdminDashboardCard.jsx
@@ -56,9 +56,9 @@ export default function AdminDashboardCard({
               <div className="font-bold text-sm">{title}</div>
               <div className=" text-[11px]">{name}</div>
               {vet ? <div className="">
-                <div className="text-sm font-bold">Email: {email}</div>
-                <div className="text-sm font-bold">Phone: {phone}</div>
-                <div className="text-sm font-bold">Vet Number: {vet_number}</div>
+                <div className="text-xs ">Email: {email}</div>
+                <div className="text-xs ">Phone: {phone}</div>
+                <div className="text-xs font-bold">Vet Number: {vet_number}</div>
               </div> : ""}
               {duration ? <div className=" text-[11px]">{duration}</div> : ""}
             </div>
