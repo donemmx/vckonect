412db9d4151e3b7aad65377fce94738c39eec390
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-09-20 11:07:34 +0100

    fix

--- src/views/admin/AdminAccountDetails.jsx
+++ src/views/admin/AdminAccountDetails.jsx
@@ -126,7 +126,7 @@ export default function AdminAccountDetails() {
       <div className=" bg-white h-[140vh] w-full mb-10 rounded-md border-[1px] border-[#EBEBEB]">
         <Link
         to='/admin-account'
-          className="flex items-center gap-3 text-[.75rem] lg:text-[.9rem] cursor-pointer ml-10 mt-10"
+          className="flex  items-center gap-3 text-[.75rem] lg:text-[.9rem] cursor-pointer ml-10 mt-10 md:mt-0"
         >
           <i className="pi pi-angle-left p-1 lg:p-3 h-[25px] w-[25px] lg:h-[45px] lg:w-[45px] bg-white rounded-full"></i>
           Back
