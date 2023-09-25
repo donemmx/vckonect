d72ce96b92db0cc6279e1dc634ea8c5737d67e2b
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-09-25 14:58:23 +0100

    fix

--- src/components/image/ImageComponent.jsx
+++ src/components/image/ImageComponent.jsx
@@ -1,31 +1,18 @@
-import { useEffect, useState } from "react";
-import { getUserById } from "../../utils/userApiService";
 
 export default function ImageComponent({ data }) {
-  const [userInfo, setUserInfo] = useState(null);
 
-  useEffect(() => {
-    const payload = {
-      id: data.user_id,
-      role: data.role,
-    };
-    console.log(data);
-    getUserById(payload).then(({data}) => {
-      setUserInfo(data);
-    });
-  }, []);
   return (
     <div className="flex items-center gap-2 py-4">
       <img
-        src={userInfo?.profile_picture}
+        src={data?.profile_picture}
         alt=""
         className="h-[40px] w-[40px] rounded-full border-2 border-green-700"
       />
       <div className=" flex flex-col ">
         <div className="text-sm">
-          {userInfo?.first_name + " " + userInfo?.last_name}
+          {data?.first_name + " " + data?.last_name}
         </div>
-        <small className=" font-light text-[11px]">{userInfo?.role === 'Animal Owner' ? 'User': userInfo?.role }</small>
+        <small className=" font-light text-[11px]">{data?.role === 'Animal Owner' ? 'User': data?.role }</small>
       </div>
     </div>
   );
