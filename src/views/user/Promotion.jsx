02849947e39f9d0d8e82f9ee3893082645466e44
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-10-01 17:55:03 +0100

    fix

--- src/views/user/Promotion.jsx
+++ src/views/user/Promotion.jsx
@@ -90,7 +90,7 @@ export default function Promotion() {
   }, [currentData]);
 
   return (
-    <div className=" flex flex-wrap gap-6">
+    <div className=" flex flex-wrap gap-4">
       <div className="activity mt-5  mb-5 p-5 lg:p-10 border bg-white rounded-lg w-full lg:w-full">
         <div className="flex items-center gap-6">
           <h2 className="text-[1rem] lg:text-[1.3rem] cursor-pointer font-black">
